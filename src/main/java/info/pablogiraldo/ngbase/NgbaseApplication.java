f495ef2c620aa426e272148007b0287b2ee8713b
Pablo Giraldo <pablogirald@yahoo.es>
2020-06-18 11:24:43 +0200

    Add base files

---
+++ src/main/java/info/pablogiraldo/ngbase/NgbaseApplication.java
@@ -0,0 +1,13 @@
+package info.pablogiraldo.ngbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgbaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/pablogiraldo/ngbase/NgbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package info.pablogiraldo.ngbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
